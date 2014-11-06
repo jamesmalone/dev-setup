dfb66fec201256b896e5625fa33c433675765ce3
Joachim Baran <joachim.baran@gmail.com>
2014-11-06 15:50:49 -0800

    Added cmake to the installation list. Removed version numbers, so latest stable version of packages will do for now.

--- install-ubuntu-14-04.sh
+++ install-ubuntu-14-04.sh
@@ -3,5 +3,7 @@
 sudo apt-get -y install vim
 sudo apt-get -y install git
 sudo apt-get -y install tmux
-sudo apt-get -y install automake1.11
-sudo apt-get -y install clang-3.5
+sudo apt-get -y install automake
+sudo apt-get -y install clang
+sudo apt-get -y install cmake
+
