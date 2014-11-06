33a74a25ef2a0817685c0ef364439e913a6bb41e
Joachim Baran <joachim.baran@gmail.com>
2014-11-06 11:45:14 -0800

    Added basic install script for Ubuntu based systems.

---
+++ install-ubuntu-14-04.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo apt-get -y install vim
+sudo apt-get -y install git
+sudo apt-get -y install tmux
+sudo apt-get -y install automake1.11
+sudo apt-get -y install clang-3.5
